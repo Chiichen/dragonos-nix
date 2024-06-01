a6e808f691100e0319ccecaf41573cd2442f4c56
Chiichen <chiichen@qq.com>
2024-06-01 16:57:25 +0000

    init commit

---
+++ devenv.nix
@@ -0,0 +1,36 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+
+
+  # https://devenv.sh/packages/
+  packages = with pkgs;[
+    git
+  ];
+
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  languages = {
+    rust = {
+      enable = true;
+      channel = "nightly";
+    };
+    c.enable = true;
+  };
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
